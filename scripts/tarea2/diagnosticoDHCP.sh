9c9478d72aca69e108c5be218c9a0afa7a123ec2
andres <anreszamorano8@gmail.com>
2026-02-15 17:59:56 -0700

    .

--- scripts/tarea2/diagnosticoDHCP.sh
+++ scripts/tarea2/diagnosticoDHCP.sh
@@ -21,9 +21,8 @@ diagnosticoDHCP(){
 
     if sudo test -f "$archivo_leases" ; then
         # Verificamos si el archivo tiene más que solo el encabezado
-		local total_lineas=$(sudo cat "$archivo_leases" 2>/dev/null | wc -l)
-        local concesiones=$((total_lineas - 1))  # Restar encabezado
-        if [ "$concesiones" -lt 1 ]; then
+		local concesiones=$(sudo grep -c -v "^address,hwaddr" "$archivo_leases" 2>/dev/null)
+        if [ "$concesiones" -eq 0 ]; then
             # Formateamos la salida: 
             # address, hwaddr, client_id, valid_lifetime, expire, subnet_id
             echo "IP_ADDRESS      MAC_ADDRESS         EXPIRACION"
