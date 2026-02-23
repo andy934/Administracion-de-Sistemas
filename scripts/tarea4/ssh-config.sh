7f16ee794747cc0759b9fc25248aa6615e59835f
andres <anreszamorano8@gmail.com>
2026-02-22 21:59:59 -0700

    Script instalcion SSH

---
+++ scripts/tarea4/ssh-config.sh
@@ -0,0 +1,28 @@
+#!/bin/bash
+
+echo "---------------------------------------------------"
+echo "-- Opciones                                      --"
+echo "-- 1. Instalar SSH                               --"
+echo "-- 2. Status                                     --"
+echo "-- 3. Salir                                      --"
+echo "---------------------------------------------------"
+
+read -p "Seleccione una opcion: " op
+
+case $op in
+    1)
+        echo "Instalando SSH..."
+        sudo dnf install openssh-server -y
+        echo "SSH instalado correctamente."
+        ;;
+    2)
+        echo "Verificando el estado del servicio SSH..."
+        sudo systemctl status ssh
+        ;;
+    3)
+        exit 0
+        ;;
+    *)
+        echo "Opcion no valida. Por favor, seleccione una opcion del 1 al 3."
+        ;;
+esac
