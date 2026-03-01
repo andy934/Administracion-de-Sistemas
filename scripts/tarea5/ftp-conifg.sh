d86eedf4686cc3febb8ee72e67f2829e1a6d6109
andres <anreszamorano8@gmail.com>
2026-02-28 19:24:41 -0700

    Creacion del Menu

--- scripts/tarea5/ftp-conifg.sh
+++ scripts/tarea5/ftp-conifg.sh
@@ -1,1 +1,19 @@
 #!/bin/bash
+. ./instalacion.sh
+. ./alta-baja.sh
+
+echo "	------------------------"
+echo "	-- CONFIGURACIÓN FTP  --"
+echo "	------------------------"
+echo ""
+echo "1. Instalar y Configurar el servidor FTP (vsftpd)"
+echo "2. Ver estado del servicio vsftpd"
+echo "3. Alta de Usuario FTP"
+echo "4. Baja de Usuario FTP"
+echo "5. Cambio de grupo del Usuario FTP"
+echo "6. Lista de Usuarios FTP registrados"
+echo "7. Ver permisos de un Usuario FTP"
+echo "8. Reiniciar el servicio vsftpd"
+echo "9. Salir"
+echo ""
+read -p "Seleccione una opción: " opcion
